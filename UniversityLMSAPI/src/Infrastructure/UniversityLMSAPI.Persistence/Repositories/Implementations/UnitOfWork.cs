930466ea271ec0f9008a530ff1044d4dee697fc6
TuranHuseynov0 <turanth-apa104@gmail.com>
2026-08-15 00:45:32 +0400

    GenericRepo and UoW

---
+++ UniversityLMSAPI/src/Infrastructure/UniversityLMSAPI.Persistence/Repositories/Implementations/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityLMSAPI.Persistence.Data;
+using UniversityLMSAPI.Persistence.Repositories.Interface;
+
+namespace UniversityLMSAPI.Persistence.Repositories.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
